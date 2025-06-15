fc542fcdb73757b0e08678fcaf014659c03ba6f1
Elliot Roberts <elliot.roberts@gmail.com>
2025-06-15 17:36:31 +0100

    set up folder structure

--- src/CommentingLayer.jsx
+++ src/CommentingLayer.jsx
@@ -666,14 +666,6 @@ export default function CommentingLayer() {
         onToggleShowComments={() => setShowComments(!showComments)}
         commentCount={comments.length}
       />
-
-      {/* Comment mode indicator */}
-      {commentMode && (
-        <div className="fixed bottom-4 left-4 bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg">
-          <MessageCircle size={16} className="inline mr-2" />
-          Comment mode active - click anywhere to add a comment
-        </div>
-      )}
     </div>
   );
 }
